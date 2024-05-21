f3fb87996de9cbf8f99d6b5dd0a9946955a10141
LMH <leemh9256@gmail.com>
2024-05-21 21:44:03 +0900

    부트스트랩 설치 // 쇼핑몰 레이아웃 설정

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
+import shoes1 from './img/shoes1.jpg'
+import clothe1 from './img/clothe1.jpg'
+import watch1 from './img/watch1.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">LMHShop</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#cart">Cart</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+        <div className={"main-bg"}/>
+        <div></div>
+        <Container>
+            <Row>
+                <Col>
+                    <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width={"80%"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </Col>
+                <Col>
+                    <img src={"https://codingapple1.github.io/shop/shoes2.jpg"} width={"80%"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </Col>
+                <Col>
+                    <img src={"https://codingapple1.github.io/shop/shoes3.jpg"} width={"80%"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </Col>
+            </Row>
+        </Container>
     </div>
   );
 }
